cd36bcec26b6457a3e706998fc251d89a42e8fc8
batuhankaan <bkaanaribali@gmail.com>
2020-08-28 12:19:57 +0300

    up

--- js/init.js
+++
@@ -1,8 +0,0 @@
-var ac = new EmlakPosApiClient();
-var app = new EmlakPosApp();
-$(document).ready(function () {
-    app.init();
-    $('.navbar-collapse a').click(function () {
-        $(".navbar-collapse").collapse('hide');
-    });
-});
