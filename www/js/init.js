450d95caea7316730e60ad97036fa513a62c6cab
batuhankaan <bkaanaribali@gmail.com>
2020-09-10 16:20:24 +0300

    up

--- www/js/init.js
+++ www/js/init.js
@@ -2,6 +2,12 @@ var ac = new EmlakPosApiClient();
 var ev = new EmValidator();
 var app = new EmlakPosApp();
 $(document).ready(function () {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+
+    function onBackKeyDown() {
+        alert("back button")
+        app.backButton();
+    }
     app.init();
     $('.navbar-collapse a').click(function () {
         $(".navbar-collapse").collapse('hide');
